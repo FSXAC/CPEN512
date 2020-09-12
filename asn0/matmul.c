65fec6ad79f5d83c91c3c8a328d8ca22a2ff63ee
Muchen He <i@muchen.ca>
2020-09-12 13:02:53 -0600

    Updated code

--- asn0/matmul.c
+++ asn0/matmul.c
@@ -2,9 +2,15 @@
 #include <stdlib.h>
 #include <time.h>
 
-// #define USE_INT
-// #define USE_FLOAT
-// #define USE_DOUBLE
+#define PRINT_MATRIX
+
+#if !defined(USE_INT) && !defined(USE_FLOAT) && !defined(USE_DOUBLE)
+#define USE_INT
+#endif
+
+#if !defined(N_SIZE)
+#define N_SIZE 4096
+#endif
 
 #if defined(USE_INT)
 typedef int t;
@@ -14,11 +20,11 @@ typedef float t;
 typedef double t;
 #endif
 
-t* mat_init (int N) {
+t* mat_init (void) {
 	int idx = 0;
-	t* matrix = (t*)malloc(sizeof(t) * N * N);
+	t* matrix = (t*)malloc(sizeof(t) * N_SIZE * N_SIZE);
 
-	while (idx <  N * N)
+	while (idx <  N_SIZE * N_SIZE)
 	{
 		#if defined(USE_INT)
 		matrix[idx++] = rand() % 10;
@@ -30,60 +36,76 @@ t* mat_init (int N) {
 	return matrix;
 }
 
-void matmul(t* A, t* B, t* C, int N)
+void matmul(t* A, t* B, t* C)
 {
 	int row, col, i;
 	t sum;
 
-	for (row = 0; row < N; row++) {
-		for (col = 0; col < N; col++) {
+	for (row = 0; row < N_SIZE; row++) {
+		for (col = 0; col < N_SIZE; col++) {
 			sum = 0;
-			for (i = 0; i < N; i++) {
-				sum += A[row * N + i] * B[i * N + col];
+			for (i = 0; i < N_SIZE; i++) {
+				sum += A[row * N_SIZE + i] * B[i * N_SIZE + col];
 			}
 			
-			C[row * N + col] = sum;
+			C[row * N_SIZE + col] = sum;
 		}
 	}
 }
 
-void print_mat(t* A, int N)
+void print_mat(t* A)
 {
 	int row = 0;
 	int col = 0;
 
-	printf("Matrix: \n");
+	if (N_SIZE <= 128) {
+		printf("Matrix: \n");
 
-	for (row = 0; row < N; row++, printf("\n")) {
-		for (col = 0; col < N; col++) {
-			#if defined(USE_INT)
-			printf("%6d", A[row * N + col]);
-			#else
-			printf("%6.2f", A[row * N + col]);
-			#endif
+		for (row = 0; row < N_SIZE; row++, printf("\n")) {
+			for (col = 0; col < N_SIZE; col++) {
+				#if defined(USE_INT)
+				printf("%6d", A[row * N_SIZE + col]);
+				#else
+				printf("%6.2f", A[row * N_SIZE + col]);
+				#endif
+			}
 		}
+	} else {
+		printf("Matrix (first 5 elements): \n");
+
+		for (row = 0; row * N_SIZE + col < 5; row++, printf("\n")) {
+			for (col = 0; row * N_SIZE + col < 5; col++) {
+
+				#if defined(USE_INT)
+				printf("%6d", A[row * N_SIZE + col]);
+				#else
+				printf("%6.2f", A[row * N_SIZE + col]);
+				#endif
+			}
+		}
+		printf("... (truncated)\n");
 	}
 }
 
-clock_t matmul_benchmark(int N)
+clock_t matmul_benchmark()
 {
 	t *A, *B, *C;
 	
-	A = mat_init(N);
-	B = mat_init(N);
-	C = mat_init(N);
+	A = mat_init();
+	B = mat_init();
+	C = mat_init();
 
-	#ifdef DEBUG_PRINT
-	print_mat(A, N);
-	print_mat(B, N);
+	#ifdef PRINT_MATRIX
+	print_mat(A);
+	print_mat(B);
 	#endif
 
 	clock_t start = clock();
-	matmul(A, B, C, N);
+	matmul(A, B, C);
 	clock_t end = clock();
 
-	#ifdef DEBUG_PRINT
-	print_mat(C, N);
+	#ifdef PRINT_MATRIX
+	print_mat(C);
 	#endif
 
 	free(A);
@@ -94,14 +116,10 @@ clock_t matmul_benchmark(int N)
 }
 
 int main(void) {
-	int n;
-
-	// printf("Enter size (N): ");
-	scanf("%d", &n);
-
-	clock_t time = matmul_benchmark(n);
-	// printf("It took %lu clock ticks (%.6fs)\n", time, (double) time / CLOCKS_PER_SEC );
-	printf("%.8f\n", (double) time / CLOCKS_PER_SEC );
+	printf("N_SIZE = %d\n", N_SIZE);
+	printf("CLOCK_PER_SEC = %d\n", CLOCKS_PER_SEC);
+	clock_t time = matmul_benchmark();
+	printf("\nCLOCK=%lu\n", time);
 
 	return 0;
 }
