f61c3d3b36b13a0ecb470acbb18fbf95a92dbf3c
Muchen He <i@muchen.ca>
2020-10-12 12:39:11 -0700

    modified compile flags

--- asn1/pt_compile.sh
+++ asn1/pt_compile.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 # Run with optimizer
-gcc -pthread ref_pthread.c -o ref_pthread.out -O1 -finline-functions -funroll-loops -m64 -D M=$1 -D N=$1 -D NUM_THREADS=$2
+gcc -pthread ref_pthread.c -o ref_pthread.out -O3 \
+-m64 -march=barcelona -mtune=barcelona -ftree-vectorize -finline-functions \
+-fmerge-all-constants -fmodulo-sched -faggressive-loop-optimizations \
+-floop-interchange -funroll-all-loops -ftree-loop-distribution -funswitch-loops \
+-D M=$1 -D N=$1 -D NUM_THREADS=$2
 
 # Debug
 # gcc -pthread ref_pthread.c -o ref.out -g -D M=$1 -D N=$1 && gdb ./ref.out
