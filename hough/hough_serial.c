c1a07c223f97e3d8eeff5b14b09b0bfe1a419d82
Muchen He <i@muchen.ca>
2020-12-14 16:06:01 -0700

    cleanup

--- hough/hough_serial.c
+++ hough/hough_serial.c
@@ -1,7 +1,5 @@
-#ifndef SERIAL_H
-#define SERIAL_H
-
 #include "common.h"
+#include "serial.h"
 
 double hough_serial(uint8_t *img, float *acc, int acc_width, int acc_height)
 {
@@ -44,4 +42,44 @@ double hough_serial(uint8_t *img, float *acc, int acc_width, int acc_height)
     return TIME(begin, end);
 }
 
-#endif
+int main() {
+
+    /* Read image */
+    int width, height, bpp;
+    uint8_t* bin_image = stbi_load(IMG_FILE, &width, &height, &bpp, 1);
+    printf("Image size: %d px by %d px, bpp: %d\n", width, height, bpp);
+    
+    if (width != IMG_SIZE || height != IMG_SIZE)
+    {
+        printf("Error! invalid image size\n");
+        return 1;
+    }
+
+    /* Set up accumulator */
+    /* Using float because int could run into overflow issues */
+    int acc_width = THETA_STEPS;
+    int acc_height = 2 * MAX_R;
+    float *acc = (float *) malloc(sizeof(float) * acc_height * acc_width);
+
+    // For each radius
+    printf("transforming to %d by %d acc...\n", acc_width, acc_height);
+    hough_serial(bin_image, acc, acc_width, acc_height);
+    
+    /* Normalize and out */
+    uint8_t* out_acc = (uint8_t *) malloc(sizeof(uint8_t) * acc_height * acc_width);
+    printf("normalizing and copying output...\n");
+    float max = normalize_image(acc, out_acc, acc_width, acc_height);
+    printf("maximum value in acc: %.1f\n", max);
+
+    /* Write out image to file */
+    stbi_write_jpg("out.jpg", acc_width, acc_height, 1, out_acc, 90);
+
+    /* Close image */
+    stbi_image_free(bin_image);
+
+    /* Free memory */
+    free(out_acc);
+    free(acc);
+
+    return 0;
+}
---
+++ hough/run_cl.sh
@@ -0,0 +1,1 @@
+gcc -arch x86_64 -o hough_opencl.out hough_opencl.c -framework OpenCL -g -D DEBUG 
